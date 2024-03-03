7bea0d07c24c3a199131daa38c8cf3db992b702c
Karthik <86306862+Karthikg1908@users.noreply.github.com>
2024-03-03 08:33:26 +0530

    Updated code

---
+++ hospital/hms/include/footer.php
@@ -0,0 +1,10 @@
+<footer>
+				<div class="footer-inner">
+					<div class="pull-left">
+			<span class="text-bold text-uppercase"> Hospital Management System</span>
+					</div>
+					<div class="pull-right">
+						<span class="go-top"><i class="ti-angle-up"></i></span>
+					</div>
+				</div>
+			</footer>
